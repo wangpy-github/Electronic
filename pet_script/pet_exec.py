2682dfaaf41eacdbe07f2bca83230b597caa2606
wangpy <wangpy9@163.com>
2019-11-30 20:54:36 +0800

    11.30

--- pet_script/pet_exec.py
+++ pet_script/pet_exec.py
@@ -1,5 +1,5 @@
 from pet_script.pet import Pet, login, send, recive
-
+import time
 ticket = "xQRFZoF0slMhC_ay4mBvxkxah96uuI4Vs5Q_c_c"
 goods_num = 1  # 每件商品购买的数量
 num = 2      # num件商品
@@ -15,6 +15,7 @@ while True:
     input("请确认是否已经付钱？？：")
 
     login()
+    time.sleep(1.5)
     send()
     recive(ticket)
     # input("请确认是否收货成功！！")
