83be3831eed17ff1a91d30dee182baf49da49b1e
wangpy <wangpy9@163.com>
2019-12-17 09:57:45 +0800

    12.16

--- pet_script/pet_exec.py
+++ pet_script/pet_exec.py
@@ -1,13 +1,14 @@
 import random
-from pet_script.pet import Pet, login, send, recive
 import time
+from pet import Pet, login, send, recive
+
 ticket = "xQRFZoF0slMhC_ay4mBvxkxah96uuI4Vs5Q_c_c"
 goods_num = 1  # 每件商品购买的数量
 while True:
     num = random.randint(1, 3)
     print("--本-次-商-品-数-量--:", num)
 
-    # tim = random.randint(1, 4)
+    # tim = random.randint(1, 3)
     # print("等待{}分钟...".format(tim))
     # time.sleep(tim*60)
 
