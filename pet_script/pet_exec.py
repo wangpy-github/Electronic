aad1ef2a78a2b2b188d259de2287b180baeb67d1
wangpy <wangpy9@163.com>
2020-03-21 23:10:25 +0800

    3.21

--- pet_script/pet_exec.py
+++ pet_script/pet_exec.py
@@ -5,7 +5,8 @@ from pet_script.Pet.pet import Pet, login, send, recive
 ticket = "xQRFZoF0slMhC_ay4mBvxkxah96uuI4Vs5Q_c_c"
 goods_num = 1  # 每件商品购买的数量
 while True:
-    num = random.randint(1, 2)
+    # num = random.randint(1, 2)
+    num = 2
     print("--本-次-商-品-数-量--:", num)
 
     # tim = random.randint(1, 3)
@@ -22,7 +23,7 @@ while True:
 
     input("请确认是否已经付钱？？：")
 
-    login()
-    time.sleep(1.5)
-    send()
-    recive(ticket)
+    # login()
+    # time.sleep(1.5)
+    # send()
+    # recive(ticket)
